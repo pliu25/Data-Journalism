dff5873139b4e658f0978dd6c927f7822f086460
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-04-08 13:38:27 -0400

    update

--- data/format_data.py
+++ data/format_data.py
@@ -1,17 +1,22 @@
 import json
 
 
-f1 = open("life_expectancy.csv", "r")
+f1 = open("data/NYC_DogLicensing_modified.csv", "r")
 lines = f1.readlines()
+#print(lines)
 
 dictionary ={}
-
+zipcodes = []
 # Create the dictionary here
-
+for line in lines:
+    line_list = line.split(",")
+    if line_list[2] != num
+    zipcodes.append(line.split(",")[2])
+print(zipcodes)
 f1.close()
 
 #Save the json object to a file
-f2 = open("life_expectancy.json", "w")
+f2 = open("data/NYC_DogLicensing_modified.json", "w")
 json.dump(dictionary, f2, indent = 4)
 
 f2.close()
