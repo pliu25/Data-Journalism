3efa5e5bd0b356c8116b74612260cb09680e9011
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-04-09 21:00:43 -0400

    update

--- data/format_data.py
+++ data/format_data.py
@@ -1,5 +1,5 @@
 import json
-
+from collections import defaultdict
 
 f1 = open("data/NYC_DogLicensing_modified.csv", "r")
 lines = f1.readlines()
@@ -7,16 +7,14 @@ lines = f1.readlines()
 
 zipcode_dict ={}
 zipcodes = []
-zipcode_breed_dict = {}
 # Create the zipcode_dict here
 for line in lines:
     line_list = list(line.split(","))
     if line_list[2][0].isnumeric() == False:
         line_list.remove(line_list[2])
     zipcodes.append(line_list[2].strip())
-    zipcode_breed_dict[line_list[2].strip()] = []
 #print(zipcodes)
-print(zipcode_breed_dict)
+#print(zipcode_breed_dict)
 
 #zipcode dictionary: # of breeds in zipcode 
 for key in zipcodes:
@@ -33,10 +31,29 @@ for line in lines:
 zipcodes_set = set(zipcodes)
 #print(zipcodes_set)
 
+zipcodes_breed_draft = defaultdict(list)
+zipcodes_breed_draft[10].append("yay")
+print(zipcodes_breed_draft)
+
+for line in lines:
+    zipcodes_breed_draft_dict = {}
+    line_list = list(line.split(","))
+    if line_list[2][0].isnumeric() == False:
+        line_list.remove(line_list[2])
+    breed_list = list(line_list[1])
+    revised_breed_list = []
+    for char in breed_list:
+        fix_char = list("".join(char.split('/', 1)[0]))
+        revised_breed_list.append(fix_char)
+        if (fix_char.isspace() == False) and (fix_char.isalpha() == False):
+            revised_breed_list.remove(fix_char)
+    zipcodes_breed_draft[line_list[2].strip()].append("".join(revised_breed_list))
+    #zipcodes_breed_draft.append(zipcodes_breed_draft_dict)
+    #zipcodes_breed_draft_dict = {}
+
+print(zipcodes_breed_draft)
 
 
-
-#print(zipcode_breed_dict)
 
 '''
 breeds_dictionary = {}
@@ -55,5 +72,6 @@ f1.close()
 #Save the json object to a file
 f2 = open("data/NYC_DogLicensing_modified.json", "w")
 json.dump(zipcode_dict, f2, indent = 4)
+json.dump(zipcodes_breed_draft, f2, indent = 4)
 
 f2.close()
