eafea3bc211807dacfaaf94f9b7d23433899b571
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-04-29 12:06:35 -0400

    update

--- server.py
+++ server.py
@@ -24,8 +24,26 @@ def macro():
 
     requested_zip = request.args.get("zip")
     all_zips = sorted(list(data["macro"].keys()))
+    
+    zip_pop = data["macro"]
+    zipcode_color = {}
 
-    return render_template('macro.html', requested_zip = requested_zip, all_zips = all_zips)
+    for key, value in zip_pop.items(): 
+        if value < 1:
+            zipcode_color[key]= "white"
+        elif value <= 2:
+            zipcode_color[key] = "240, 212, 255"
+        elif value <= 98:
+            zipcode_color[key] = "223, 169, 252"
+        elif value <= 360:
+            zipcode_color[key] = "202, 109, 252"
+        elif value <= 655:
+            zipcode_color[key] = "180, 59, 245"
+        elif value <= 2380:
+            zipcode_color[key] = "129, 4, 196"
+    print(zipcode_color) 
+    #use jinja templating to pass in zip, the use javascript 
+    return render_template('macro.html', requested_zip = requested_zip, all_zips = all_zips, zipcode_color = zipcode_color)
 
 @app.route('/micro')
 def micro():
